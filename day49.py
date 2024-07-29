3ed046a40c162d19eee9e04833ed5a1450b76365
kaleycode <readingkale@gmail.com>
2024-07-29 10:52:31 -0700

    Add files via upload

---
+++ day49.py
@@ -0,0 +1,16 @@
+import os, time
+
+f = open("high.score", "r")
+scores = f.read().split("\n")
+f.close()
+
+highScore = 0
+name = None
+
+for rows in scores:
+  data = rows.split()
+  if data != []:
+    if int(data[1]) > highScore:
+      highScore = int(data[1])
+      name = data[0]
+print("The winner is", name, "with", highScore)
